d20f00dfeb91e917505a732baa3473a6adc0fabf
Klubuntu <kb.klubuntu@wp.pl>
2024-07-16 13:51:40 +0200

    Renamed styles component + add app switcher style

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './app.css';
 import App from './loader';
 import reportWebVitals from './reportWebVitals';
 
