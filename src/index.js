04b93b0d6645a4f6e2fef494c0626658c645e650
Kamil Klubuntu <49614906+Klubuntu@users.noreply.github.com>
2024-08-09 13:15:34 +0200

    Updated routing to new version
    Signed-off-by: Kamil Klubuntu <49614906+Klubuntu@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,26 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Routes, Route } from 'react-router-dom';
 import './app.css';
-import App from './loader';
+import App from './app';
 import Settings from './pages/Settings';
 import reportWebVitals from './reportWebVitals';
 
-export default function AppRouting() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<App />} />
-          {/* <Route path="contact" element={<Contact />} /> */}
-          {/* <Route path="*" element={<NoPage />} /> */}
-        </Route>
-        <Route path="/internal/app/settings" element={<Settings />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
 
+export default function AppRouting() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <App />,
+      children: [
+        {
+          index: true,
+          element: <App />,
+        },
+      ],
+    },
+    {
+      path: '/internal/app/settings',
+      element: <Settings />,
+    },
+  ]);
+  return <RouterProvider router={router} />;
+}
 
 const root = ReactDOM.createRoot(document.getElementById('web-os'));
 root.render(
@@ -29,7 +34,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
